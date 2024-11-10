9c6160a001ea0a7a655728ecfdc014be191d34e6
Owain Davies <116417456+OTheDev@users.noreply.github.com>
2024-11-10 14:52:58 +0700

    Minor adjustments to the detect people/faces examples (#7)
    * Also updates the README to include the new detect people example.

--- examples/detect_faces/src/main.rs
+++ examples/detect_faces/src/main.rs
@@ -5,18 +5,23 @@ SPDX-License-Identifier: Apache-2.0 OR MIT
 
 use az_analyze_image::v32::client::{AnalyzeImageOptions, Client};
 use az_analyze_image::v32::{FaceDescription, VisualFeatureTypes};
-
 use image::{load_from_memory_with_format, ImageFormat, Rgb, RgbImage};
 use imageproc::drawing::draw_hollow_rect_mut;
 use imageproc::rect::Rect;
-
 use reqwest::Client as ReqwestClient;
-
 use std::env::var;
 use std::error::Error;
 
 const IMAGE_URL: &str =
     "https://images.pexels.com/photos/1367269/pexels-photo-1367269.jpeg";
+
+// Pixel thickness for rectangle boundaries.
+const THICKNESS: u32 = 8;
+
+// Colors for each rectangle boundary. If there's more faces than COLORS.len(),
+// then we wrap around.
+const COLORS: &[Rgb<u8>] =
+    &[Rgb([1, 255, 79]), Rgb([0, 237, 245]), Rgb([118, 232, 181])];
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -25,17 +30,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Call the Analyze Image API with the face detection feature enabled
     let faces: Vec<FaceDescription> = detect_faces(&client, IMAGE_URL).await?;
 
-    // Download the image bytes
+    // Download and Load the image
     let img_bytes: Vec<u8> = download_image(IMAGE_URL).await?;
-
-    // Load the image
     let mut img: RgbImage =
         load_from_memory_with_format(&img_bytes, ImageFormat::Jpeg)?.to_rgb8();
 
     // Draw bounding boxes around all detected faces
-    draw_face_bounding_boxes(&mut img, &faces);
+    draw_bounding_boxes(&mut img, &faces, COLORS, THICKNESS);
 
-    // Save the new image
     img.save("out.jpg")?;
 
     Ok(())
@@ -45,7 +47,7 @@ fn get_client() -> Client {
     let key = var("CV_KEY").expect("no CV_KEY");
     let endpoint = var("CV_ENDPOINT").expect("no CV_ENDPOINT");
 
-    Client::new(key, &endpoint).expect("failed to create client")
+    Client::new(key, &endpoint).unwrap()
 }
 
 async fn detect_faces(
@@ -57,7 +59,6 @@ async fn detect_faces(
         visual_features: Some(&features),
         ..Default::default()
     };
-
     let analysis = client.analyze_image_url(url, options).await?;
 
     Ok(analysis.faces.unwrap())
@@ -66,20 +67,22 @@ async fn detect_faces(
 async fn download_image(url: &str) -> Result<Vec<u8>, Box<dyn Error>> {
     let client = ReqwestClient::new();
     let response = client.get(url).send().await?.bytes().await?;
+
     Ok(response.to_vec())
 }
 
-fn draw_face_bounding_boxes(img: &mut RgbImage, faces: &[FaceDescription]) {
-    let thickness = 16;
-    let colors = [Rgb([1, 255, 79]), Rgb([0, 237, 245]), Rgb([118, 232, 181])];
-
+fn draw_bounding_boxes(
+    img: &mut RgbImage,
+    faces: &[FaceDescription],
+    colors: &[Rgb<u8>],
+    thickness: u32,
+) {
     for (i, face) in faces.iter().enumerate() {
         let bounding_box = Rect::at(
             face.face_rectangle.left as i32,
             face.face_rectangle.top as i32,
         )
         .of_size(face.face_rectangle.width, face.face_rectangle.height);
-
         let color = colors[i % colors.len()];
 
         draw_thick_rect(img, bounding_box, color, thickness);
@@ -90,11 +93,11 @@ fn draw_thick_rect(
     img: &mut RgbImage,
     rect: Rect,
     color: Rgb<u8>,
-    thickness: i32,
+    thickness: u32,
 ) {
     for i in 0..thickness {
-        let new_rect = Rect::at(rect.left() - i, rect.top() - i)
-            .of_size(rect.width() + i as u32 * 2, rect.height() + i as u32 * 2);
+        let new_rect = Rect::at(rect.left() - i as i32, rect.top() - i as i32)
+            .of_size(rect.width() + i * 2, rect.height() + i * 2);
 
         draw_hollow_rect_mut(img, new_rect, color);
     }
--- examples/detect_people/src/main.rs
+++ examples/detect_people/src/main.rs
@@ -5,18 +5,23 @@ SPDX-License-Identifier: Apache-2.0 OR MIT
 
 use az_analyze_image::v40::client::{AnalyzeImageOptions, Client};
 use az_analyze_image::v40::{DetectedPerson, VisualFeature};
-
 use image::{load_from_memory_with_format, ImageFormat, Rgb, RgbImage};
 use imageproc::drawing::draw_hollow_rect_mut;
 use imageproc::rect::Rect;
-
 use reqwest::Client as ReqwestClient;
-
 use std::env::var;
 use std::error::Error;
 
 const IMAGE_URL: &str =
     "https://images.pexels.com/photos/1367269/pexels-photo-1367269.jpeg";
+
+// Pixel thickness for rectangle boundaries.
+const THICKNESS: u32 = 8;
+
+// Colors for each rectangle boundary. If there's more people than COLORS.len(),
+// then we wrap around.
+const COLORS: &[Rgb<u8>] =
+    &[Rgb([1, 255, 79]), Rgb([0, 237, 245]), Rgb([118, 232, 181])];
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -25,17 +30,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Call the Analyze Image API with the people detection feature enabled
     let people: Vec<DetectedPerson> = detect_people(&client, IMAGE_URL).await?;
 
-    // Download the image bytes
+    // Download and Load the image
     let img_bytes: Vec<u8> = download_image(IMAGE_URL).await?;
-
-    // Load the image
     let mut img: RgbImage =
         load_from_memory_with_format(&img_bytes, ImageFormat::Jpeg)?.to_rgb8();
 
     // Draw bounding boxes around detected persons with confidence > 0.75
-    draw_people_bounding_boxes(&mut img, &people);
+    draw_bounding_boxes(&mut img, &people, COLORS, THICKNESS);
 
-    // Save the new image
     img.save("out.jpg")?;
 
     Ok(())
@@ -45,7 +47,7 @@ fn get_client() -> Client {
     let key = var("CV_KEY").expect("no CV_KEY");
     let endpoint = var("CV_ENDPOINT").expect("no CV_ENDPOINT");
 
-    Client::new(key, &endpoint).expect("failed to create client")
+    Client::new(key, &endpoint).unwrap()
 }
 
 async fn detect_people(
@@ -57,22 +59,24 @@ async fn detect_people(
         features: Some(&features),
         ..Default::default()
     };
-
     let analysis = client.analyze_image_url(url, options).await?;
 
-    Ok(analysis.people_result.expect("no people result").values)
+    Ok(analysis.people_result.unwrap().values)
 }
 
 async fn download_image(url: &str) -> Result<Vec<u8>, Box<dyn Error>> {
     let client = ReqwestClient::new();
     let response = client.get(url).send().await?.bytes().await?;
+
     Ok(response.to_vec())
 }
 
-fn draw_people_bounding_boxes(img: &mut RgbImage, people: &[DetectedPerson]) {
-    let thickness = 16;
-    let colors = [Rgb([1, 255, 79]), Rgb([0, 237, 245]), Rgb([118, 232, 181])];
-
+fn draw_bounding_boxes(
+    img: &mut RgbImage,
+    people: &[DetectedPerson],
+    colors: &[Rgb<u8>],
+    thickness: u32,
+) {
     for (i, person) in people.iter().enumerate() {
         if person.confidence <= 0.75 {
             continue;
@@ -83,7 +87,6 @@ fn draw_people_bounding_boxes(img: &mut RgbImage, people: &[DetectedPerson]) {
             person.bounding_box.y as i32,
         )
         .of_size(person.bounding_box.w, person.bounding_box.h);
-
         let color = colors[i % colors.len()];
 
         draw_thick_rect(img, bounding_box, color, thickness);
@@ -94,11 +97,11 @@ fn draw_thick_rect(
     img: &mut RgbImage,
     rect: Rect,
     color: Rgb<u8>,
-    thickness: i32,
+    thickness: u32,
 ) {
     for i in 0..thickness {
-        let new_rect = Rect::at(rect.left() - i, rect.top() - i)
-            .of_size(rect.width() + i as u32 * 2, rect.height() + i as u32 * 2);
+        let new_rect = Rect::at(rect.left() - i as i32, rect.top() - i as i32)
+            .of_size(rect.width() + i * 2, rect.height() + i * 2);
 
         draw_hollow_rect_mut(img, new_rect, color);
     }
